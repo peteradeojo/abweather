ee96573cfb1342c7050fc4e74d00ddb1b3cc6d6d
peteradeojo <60135915+peteradeojo@users.noreply.github.com>
2020-09-13 18:05:53 +0100

    removed notifications and shitt

--- public/index.sw.js
+++ public/index.sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "ABWEATHER-V2";
-const version = "abweather-v2";
+const version = "abweather-v3";
 
 const urlsToCache = [
 	'/',
@@ -23,7 +23,6 @@ self.addEventListener('install', installer => {
 	};
 
 	installer.waitUntil(done());
-	self.skipWaiting();
 });
 
 self.addEventListener('fetch', fetchEvent => {
@@ -71,30 +70,4 @@ self.addEventListener('activate', activator => {
 	};
 
 	activator.waitUntil(done());
-});
-
-self.registration.showNotification('Weather in your Location', {
-	body: "Get the weather in your location right now!",
-	image: "/assets/background.jpg",
-	icon: "/android-chrome-192x192.png",
-	badge: "/android-chrome-192x192.png",
-	actions: [
-		{action: "view", title: "View", icon: '/favicon-32x32.png'}
-	]
-});
-
-self.addEventListener('notificationclose', evt => {
-	console.log('notification closed');
-});
-
-self.addEventListener('notificationclick', evt => {
-	evt.notification.close();
-	
-	switch (evt.action) {
-		case 'view':
-			console.log('view clicked');
-			break;
-		default:
-			console.log('notification clicked');
-	}
 });
